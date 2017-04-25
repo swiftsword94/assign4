42e6b09710b3358ee8a5f34812fe259a60016141
swiftsword94 <swiftsword94@gmail.com>
2017-04-25 11:53:41 -0400

    Fixed actions added to nodes

--- src/valueIter/Driver.java
+++ src/valueIter/Driver.java
@@ -15,11 +15,18 @@ public class Driver
 		s1.addAction(new Node[]{s1, s4}, new Double[]{0.2, 0.8});
 		s2.addAction(new Node[]{s2, s3}, new Double[]{0.2, 0.8});
 		s2.addAction(new Node[]{s2, s1}, new Double[]{0.2, 0.8});
-		s1.addAction(new Node[]{s1, s2}, new Double[]{0.2, 0.8});
+		s3.addAction(new Node[]{s2}, new Double[]{1.0});
+		s3.addAction(new Node[]{s4}, new Double[]{1.0});
+		s4.addAction(new Node[]{s4, s3}, new Double[]{0.1, 0.9});
+		s4.addAction(new Node[]{s4, s1}, new Double[]{0.2, 0.8});
+		
+		graph.addNode(s1);
+		graph.addNode(s2);
+		graph.addNode(s3);
+		graph.addNode(s4);
 	}
 	public static void main(String[] args)
 	{
-		
+		new Driver().test();
 	}
-
 }
